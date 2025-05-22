ff6c33637b8c0fc254328d3cb3fd73af46cd84f5
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2025-05-22 00:05:26 -0700

    First commit

---
+++ src/main/java/net/nithin/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.nithin.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
