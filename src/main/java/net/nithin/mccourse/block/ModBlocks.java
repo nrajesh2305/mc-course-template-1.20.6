7140077d42b7b4f9837e551fd482c6819b7f1e5f
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2025-05-27 20:32:10 -0700

    Learned how to add/create custom loot tables in game.

--- src/main/java/net/nithin/mccourse/block/ModBlocks.java
+++ src/main/java/net/nithin/mccourse/block/ModBlocks.java
@@ -1,16 +1,14 @@
 package net.nithin.mccourse.block;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
+//import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.nithin.mccourse.MCCourseMod;
 
 public class ModBlocks {
@@ -24,14 +22,16 @@ public class ModBlocks {
             ));
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(Block.Settings.create()
-                    .mapColor(MapColor.IRON_GRAY)
-                    .strength(5.0f, 6.0f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.METAL)
+                        .mapColor(MapColor.IRON_GRAY)
+                        .strength(5.0f, 6.0f)
+                        .requiresTool()
+                        .sounds(BlockSoundGroup.METAL)
             ));
 
     public static final Block DEEPSLATE_PINK_GARNET_ORE = registerBlock("deepslate_pink_garnet_ore",
-            new Block(Block.Settings.create()
+            new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    Block.Settings.create()
                     .mapColor(MapColor.DEEPSLATE_GRAY)
                     .strength(3.0f, 6.0f)
                     .requiresTool()
@@ -40,7 +40,9 @@ public class ModBlocks {
             ));
 
     public static final Block NETHER_PINK_GARNET_ORE = registerBlock("nether_pink_garnet_ore",
-            new Block(Block.Settings.create()
+            new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    Block.Settings.create()
                     .mapColor(MapColor.DARK_RED)
                     .strength(0.4f, 0.4f)
                     .requiresTool()
@@ -49,7 +51,9 @@ public class ModBlocks {
             ));
 
     public static final Block END_STONE_PINK_GARNET_ORE = registerBlock("end_stone_pink_garnet_ore",
-            new Block(Block.Settings.create()
+            new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    Block.Settings.create()
                     .mapColor(MapColor.PALE_YELLOW)
                     .strength(3.0f, 9.0f)
                     .requiresTool()
@@ -58,7 +62,9 @@ public class ModBlocks {
             ));
 
     public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
-            new Block(Block.Settings.create()
+            new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    Block.Settings.create()
                     .mapColor(MapColor.STONE_GRAY)
                     .strength(1.5f, 6.0f)
                     .requiresTool()
